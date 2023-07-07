a4647d52fee4a3839364dd225f49c111d6f8f2eb
ImNuam <118882456+ImNuam@users.noreply.github.com>
2023-07-07 14:41:25 +0900

    first commit

---
+++ src/main/java/com/likelion/honeyBottle/HoneyBottleApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.honeyBottle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoneyBottleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoneyBottleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/likelion/honeyBottle/HoneyBottleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.honeyBottle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoneyBottleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
